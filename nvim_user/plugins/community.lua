93e677b2592a9ae20ae6880ac20dcb958705f772
Jim Hoskins <jim@jimhoskins.com>
2023-06-29 19:35:48 -0700

    [nvim] twilight and rainbows

--- nvim_user/plugins/community.lua
+++ nvim_user/plugins/community.lua
@@ -5,7 +5,9 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.color.twilight-nvim" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.editing-support.nvim-ts-rainbow" },
   -- { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.motion.nvim-spider" },
