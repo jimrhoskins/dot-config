ddc32cd47f8f594d4bc37b537f562416bd4e6620
Jim Hoskins <jim@jimhoskins.com>
2023-06-29 18:54:26 -0700

    [nvim] add nvim-surround

--- nvim_user/plugins/community.lua
+++ nvim_user/plugins/community.lua
@@ -6,6 +6,7 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.pack.typescript" },
 
+  { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.completion.copilot-lua" },
   { -- further customize the options set by the community
     "copilot.lua",
