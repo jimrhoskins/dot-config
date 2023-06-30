f9b44eff9006406273491aa1a23edcd98f0d38d3
Jim Hoskins <jim@jimhoskins.com>
2023-06-29 19:15:35 -0700

    [nvim] Add some motions

--- nvim_user/plugins/community.lua
+++ nvim_user/plugins/community.lua
@@ -6,7 +6,10 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.pack.typescript" },
 
+  { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.nvim-spider" },
+  { import = "astrocommunity.motion.nvim-matchup" },
   { import = "astrocommunity.completion.copilot-lua" },
   { -- further customize the options set by the community
     "copilot.lua",
