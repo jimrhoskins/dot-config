346ab63be166c35fcebf97b0772b632735e646a6
Jim Hoskins <jim.hoskins@usekojo.com>
2023-07-01 15:32:20 -0700

    Show hidden files in neo-tree

---
+++ nvim_user/plugins/neo-tree.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = function(_, opts)
+    opts.filesystem.filtered_items = {
+      visible = true,
+      hide_dotfiles = false,
+      hide_gitignored = false,
+      hide_by_name = {
+        "node_modules"
+      }
+    }
+
+    return opts
+  end,
+}
