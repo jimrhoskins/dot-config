be7ff92b80808f9a3b535358e29dd8b583b1e8e8
Jim Hoskins <jim.hoskins@usekojo.com>
2023-07-01 18:06:36 -0700

    Add mason requirements

--- nvim_user/plugins/mason.lua
+++ nvim_user/plugins/mason.lua
@@ -7,7 +7,13 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "bashls",
+        "eslint",
+        "graphql",
+        "jsonls",
         "lua_ls",
+        "prismals",
+        "tsserver",
       })
     end,
   },
@@ -18,6 +24,9 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "eslint_d",
+        "prettier",
+        "prettierd",
       })
     end,
   },
@@ -27,7 +36,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+        "js-debug-adapter",
       })
     end,
   },
