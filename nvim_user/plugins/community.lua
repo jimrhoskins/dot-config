75a9d4ada435793d9c40456224e794f8b26eabae
Jim Hoskins <jim@jimhoskins.com>
2023-07-02 14:31:21 -0700

    [nvim] -spider +mini-move

--- nvim_user/plugins/community.lua
+++ nvim_user/plugins/community.lua
@@ -11,7 +11,8 @@ return {
   -- { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
-  { import = "astrocommunity.motion.nvim-spider" },
+  { import = "astrocommunity.motion.mini-move" },
+  --  { import = "astrocommunity.motion.nvim-spider" },
   { import = "astrocommunity.motion.vim-matchup" },
   { import = "astrocommunity.completion.copilot-lua" },
   { -- further customize the options set by the community
