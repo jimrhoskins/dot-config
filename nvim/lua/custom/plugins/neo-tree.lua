ac679ea33ae4725b18d5d1dd7df09368b4e01dca
Jim Hoskins <jim@jimhoskins.com>
2024-04-13 13:09:40 -0700

    updates

--- nvim/lua/custom/plugins/neo-tree.lua
+++ nvim/lua/custom/plugins/neo-tree.lua
@@ -12,7 +12,7 @@ return {
     local command = require 'neo-tree.command'
     local keymap = vim.keymap
 
-    keymap.set('n', '<leader>ee', ':NeoTreeRevealToggle<cr>', { desc = 'Filetre[e]' })
-    keymap.set('n', '<leader>ef', ':NeoTreeFloatToggle<cr>', { desc = 'Filetre[e] [F]ocus' })
+    keymap.set('n', '<leader>ee', '<cmd>Neotree toggle left <cr>', { desc = 'Filetre[e]' })
+    keymap.set('n', '<leader>ef', '<cmd>Neotree reveal toggle left  <cr>', { desc = 'Filetre[e] [F]ocus' })
   end,
 }
