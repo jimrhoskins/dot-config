482fecb77694aa6763a7eeda7a26bc99326e62f4
Jim Hoskins <jim@jimhoskins.com>
2024-04-13 11:25:15 -0700

    style insertion doc

--- nvim/init.lua
+++ nvim/init.lua
@@ -603,12 +603,19 @@ require('lazy').setup({
       luasnip.config.setup {}
 
       cmp.setup {
+        window = {
+          documentation = {
+            border = 'rounded',
+          },
+        },
         snippet = {
           expand = function(args)
             luasnip.lsp_expand(args.body)
           end,
         },
-        completion = { completeopt = 'menu,menuone,noinsert' },
+        completion = {
+          completeopt = 'menu,menuone,noinsert',
+        },
 
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
