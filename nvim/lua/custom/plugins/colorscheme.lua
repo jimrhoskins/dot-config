8e566622cefa9e33f6eb71ed8c6ded2bb34da905
Jim Hoskins <jim@jimhoskins.com>
2024-04-13 15:36:40 -0700

    colorschemes

--- nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/lua/custom/plugins/colorscheme.lua
@@ -4,6 +4,7 @@ return { -- You can easily change to a different colorscheme.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
   'catppuccin/nvim',
+  dependencies = { 'folke/tokyonight.nvim' },
   name = 'catppuccin',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
