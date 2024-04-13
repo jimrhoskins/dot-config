93363a440e9c74b7ac01a54b91992b57b87ff285
Jim Hoskins <jim@jimhoskins.com>
2024-04-13 15:02:55 -0700

    wrappign

--- nvim/lua/custom/keymaps.lua
+++ nvim/lua/custom/keymaps.lua
@@ -2,3 +2,9 @@ local keymap = vim.keymap
 
 keymap.set('i', 'jk', '<Esc>', { silent = true, noremap = true, expr = false, nowait = false, desc = 'jk to escape' })
 keymap.set('n', 'fs', '<cmd>w<CR>', { silent = true, noremap = true, expr = false, nowait = false, desc = 'Save file' })
+
+local function toggle_wrap()
+  vim.wo.wrap = not vim.wo.wrap
+end
+
+keymap.set('n', '<leader>tw', toggle_wrap, { silent = true, noremap = true, expr = false, nowait = false, desc = 'Toggle wrap' })
---
+++ nvim/lua/custom/plugins/nvim-navic.lua
@@ -0,0 +1,13 @@
+return {
+  'SmiteshP/nvim-navic',
+  event = 'VimEnter',
+  dependencies = { 'neovim/nvim-lspconfig' },
+  config = function()
+    require('nvim-navic').setup {
+      lsp = {
+        auto_attach = true,
+      },
+    }
+    vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
+  end,
+}
