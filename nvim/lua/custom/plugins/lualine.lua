3ad3f8e1dce846676e81f7a1296fc092706b31cb
Jim Hoskins <jim@jimhoskins.com>
2024-04-13 20:31:54 -0700

    winbar / navic

--- nvim/lua/custom/plugins/lualine.lua
+++ nvim/lua/custom/plugins/lualine.lua
@@ -1,16 +1,46 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'SmiteshP/nvim-navic',
+    'neovim/nvim-lspconfig',
+  },
   config = function()
     local lualine = require 'lualine'
     local lazy_status = require 'lazy.status' -- to configure lazy pending updates count
+
+    require('nvim-navic').setup {
+      lsp = {
+        auto_attach = true,
+      },
+      separator = ' \u{e0b1} ',
+      highlight = true,
+    }
 
     -- configure lualine with modified theme
     lualine.setup {
       -- options = {
       --   theme = my_lualine_theme,
       -- },
+      winbar = {
+        lualine_c = {
+          { 'navic', separator = '' },
+          {
+            function()
+              return ' '
+            end,
+          },
+        },
+      },
       sections = {
+        lualine_a = {
+          {
+            'mode',
+            fmt = function(mode)
+              return mode:sub(1, 1):upper()
+            end,
+          },
+        },
         lualine_c = {
           {
             'filename',
