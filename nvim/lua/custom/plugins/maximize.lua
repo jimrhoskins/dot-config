2066af940d8a262970e2a524ee8c0197d5f6abdc
Jim Hoskins <jim@jimhoskins.com>
2024-04-13 18:10:20 -0700

    maximize + cleani[p

---
+++ nvim/lua/custom/plugins/maximize.lua
@@ -0,0 +1,11 @@
+return {
+  'declancm/maximize.nvim',
+  config = function()
+    local maximize = require 'maximize'
+    maximize.setup {
+      default_keymaps = false,
+    }
+
+    vim.keymap.set('n', '<leader>z', maximize.toggle, { desc = 'Maximi[z]e' })
+  end,
+}
