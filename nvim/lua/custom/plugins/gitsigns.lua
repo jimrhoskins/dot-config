ea3c519d946a658fbefd9b90ad0aefd1ae91426d
Jim Hoskins <jim@jimhoskins.com>
2024-04-13 19:55:53 -0700

    update lualine

--- nvim/lua/custom/plugins/gitsigns.lua
+++ nvim/lua/custom/plugins/gitsigns.lua
@@ -1,7 +1,6 @@
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
   opts = {
-
     signs = {
       add = { text = '┃' },
       change = { text = '┃' },
