101050a0efa06d4df3f41a4e5fc2043f40cef169
Jim Hoskins <jim@jimhoskins.com>
2024-04-13 17:21:54 -0700

    Style neo-tree

--- nvim/lua/custom/plugins/neo-tree.lua
+++ nvim/lua/custom/plugins/neo-tree.lua
@@ -8,10 +8,14 @@ return {
   },
   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
   config = function()
-    require('neo-tree').setup {}
+    require('neo-tree').setup {
+      popup_border_style = 'rounded',
+    }
+
     local keymap = vim.keymap
 
     keymap.set('n', '<leader>ee', '<cmd>Neotree toggle left <cr>', { desc = 'Filetre[e]' })
     keymap.set('n', '<leader>ef', '<cmd>Neotree reveal toggle left  <cr>', { desc = 'Filetre[e] [F]ocus' })
+    keymap.set('n', '<leader>ep', '<cmd>Neotree reveal float  <cr>', { desc = 'Filetre[e] [P]opup' })
   end,
 }
