30587a0d2fc224a7f46530d4adfa853b4c140352
Jim Hoskins <jim@jimhoskins.com>
2024-04-13 18:00:05 -0700

    cleanup

--- nvim/lua/custom/plugins/telescope.lua
+++ nvim/lua/custom/plugins/telescope.lua
@@ -19,15 +19,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
   config = function()
     -- See `:help telescope` and `:help telescope.setup()`
     require('telescope').setup {
-      -- You can put your default mappings / updates / etc. in here
-      --  All the info you're looking for is in `:help telescope.setup()`
-      --
-      -- defaults = {
-      --   mappings = {
-      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-      --   },
-      -- },
-      -- pickers = {}
       extensions = {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
