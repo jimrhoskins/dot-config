4daa13fa8f4190d94f9d00660d705ebe5bb88174
Jim Hoskins <jim.hoskins@usekojo.com>
2024-04-21 21:34:16 -0700

    Fix keymaps, add gitignore entries

--- nvim/lua/custom/keymaps.lua
+++ nvim/lua/custom/keymaps.lua
@@ -1,7 +1,9 @@
 local keymap = vim.keymap
 
 keymap.set('i', 'jk', '<Esc>', { silent = true, noremap = true, expr = false, nowait = false, desc = 'jk to escape' })
-keymap.set('n', 'fs', '<cmd>w<CR>', { silent = true, noremap = true, expr = false, nowait = false, desc = 'Save file' })
+keymap.set('i', 'jj', '<Esc>', { silent = true, noremap = true, expr = false, nowait = false, desc = 'jk to escape' })
+keymap.set('i', 'kk', '<Esc>', { silent = true, noremap = true, expr = false, nowait = false, desc = 'jk to escape' })
+keymap.set('n', '<leader>fs', '<cmd>w<CR>', { silent = true, noremap = true, expr = false, nowait = false, desc = 'Save file' })
 
 local function toggle_wrap()
   vim.wo.wrap = not vim.wo.wrap
