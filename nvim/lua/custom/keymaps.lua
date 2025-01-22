d3496cf8225dc6df7a04e2aa2b780f6feb16d6ba
Jim Hoskins <jim.hoskins@usekojo.com>
2025-01-21 21:16:04 -0800

    Add lua exec keymaps

--- nvim/lua/custom/keymaps.lua
+++ nvim/lua/custom/keymaps.lua
@@ -5,6 +5,10 @@ keymap.set('i', 'jj', '<Esc>', { silent = true, noremap = true, expr = false, no
 keymap.set('i', 'kk', '<Esc>', { silent = true, noremap = true, expr = false, nowait = false, desc = 'jk to escape' })
 keymap.set('n', '<leader>fs', '<cmd>w<CR>', { silent = true, noremap = true, expr = false, nowait = false, desc = 'Save file' })
 
+keymap.set('n', '<leader>lX', '<cmd>source %<CR>')
+keymap.set('n', '<leader>lx', ':.lua<CR>')
+keymap.set('v', '<leader>lx', ':lua<CR>')
+
 local function toggle_wrap()
   vim.wo.wrap = not vim.wo.wrap
 end
