aca35fb2eb0d9ac2e5356b67e36b4beaf2c33267
Jim Hoskins <jim@jimhoskins.com>
2025-10-03 22:17:00 -0700

    Add shell aliases

--- shell/dot-config/bash/rc.d/aliases.sh
+++ shell/dot-config/bash/rc.d/aliases.sh
@@ -1,1 +1,12 @@
 # Aliases
+#
+# alias vim to nvim if exists
+if command -v nvim &>/dev/null; then
+  alias vim='nvim'
+fi
+
+
+alias yay-add='omarchy-pkg-aur-install'
+alias yay-remove='omarchy-pkg-remove'
+alias pac-add='omarchy-pkg-install'
+alias pac-remove='omarchy-pkg-remove'
