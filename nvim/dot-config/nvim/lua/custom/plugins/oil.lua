aa7a78c959a2cfa785c73e01b4fc2d9991e38ee8
Jim Hoskins <jim@jimhoskins.com>
2025-10-13 10:43:50 -0700

    Nvim: boot to oil

--- nvim/dot-config/nvim/lua/custom/plugins/oil.lua
+++ nvim/dot-config/nvim/lua/custom/plugins/oil.lua
@@ -12,6 +12,27 @@ return {
 
       local keymap = vim.keymap
       keymap.set('n', '-', '<cmd>Oil<cr>', { desc = 'Open Oil' })
+
+      vim.api.nvim_create_autocmd('VimEnter', {
+        desc = 'Open Oil in the final cwd when starting with no args',
+        callback = function(ev)
+          if vim.fn.argc() > 0 then
+            return
+          end
+          -- let other plugins (project.nvim, etc.) finish changing cwd
+          vim.defer_fn(function()
+            local dir = vim.loop.cwd() -- final cwd after all autocmds
+            -- using the :Oil command ensures a proper refresh
+            vim.cmd('silent keepalt Oil ' .. vim.fn.fnameescape(dir))
+          end, 20)
+        end,
+      })
+      -- vim.api.nvim_create_autocmd('VimEnter', {
+      --   callback = function(data)
+      --     local dir = vim.fn.isdirectory(data.file) == 1 and data.file or vim.fn.getcwd()
+      --     require('oil').open(dir)
+      --   end,
+      -- })
     end,
   },
 }
