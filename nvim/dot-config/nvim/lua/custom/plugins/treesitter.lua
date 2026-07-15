08a16599103821a3bb73ff51bb0d665692c66b7d
Jim Hoskins <jim@jimhoskins.com>
2026-07-15 12:19:12 -0700

    Adopt latest omarchy

--- nvim/dot-config/nvim/lua/custom/plugins/treesitter.lua
+++ nvim/dot-config/nvim/lua/custom/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs',
+  main = 'nvim-treesitter.config',
   opts = {
     ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
     -- Autoinstall languages that are not installed
